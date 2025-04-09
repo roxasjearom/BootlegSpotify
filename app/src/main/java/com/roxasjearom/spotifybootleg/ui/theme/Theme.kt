257f7b54e2637b996bae85fc35bcd025759c7831
Jearom Roxas <roxasjearom@gmail.com>
2025-04-09 22:08:08 +0800

    Update theme colors

--- app/src/main/java/com/roxasjearom/spotifybootleg/ui/theme/Theme.kt
+++ app/src/main/java/com/roxasjearom/spotifybootleg/ui/theme/Theme.kt
@@ -1,57 +1,29 @@
 package com.roxasjearom.spotifybootleg.ui.theme
 
-import android.app.Activity
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.Color
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+private val SpotifyDarkColorScheme = darkColorScheme(
+    primary = SpotifyGreen,
+    onPrimary = Color.Black,
+    secondary = SpotifyLightGray,
+    onSecondary = Color.Black,
+    background = Color.Black,
+    onBackground = Color.White,
+    surface = SpotifyBlack,
+    onSurface = Color.White,
+    error = SpotifyRed,
+    onError = Color.White,
 )
 
 @Composable
 fun SpotifyBootlegTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = SpotifyDarkColorScheme,
         typography = Typography,
         content = content
     )
